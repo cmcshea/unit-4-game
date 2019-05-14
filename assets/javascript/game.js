81071d234a6e65040c641ff77740cf3b426a7bc0
cmcshea <cory.mcshea@gmail.com>
2019-05-14 14:46:37 -0400

    adding style page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,25 @@
 // Option 1 Game design notes
 
 // There will be four crystals displayed as buttons on the page.
-
+ 
+var crystal1, crystal2, crystal3, crystal4
 
 // The player will be shown a random number at the start of the game.
 
+function randomNumber() {
+    var random = Math.floor(Math.random() * 120) + 19; 
+    return random
+}
 
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 
+//onClick Event when crystal is clicked, no. of points is updated in html
+
+
+
+
 // Your game will hide this amount until the player clicks a crystal.
 // When they do click one, update the player's score counter.
-
 
 
 // The player wins if their total score matches the random number from the beginning of the game.
