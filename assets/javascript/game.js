c0b93959a2576f387182c8777f7baa4e33665515
cmcshea <cory.mcshea@gmail.com>
2019-05-16 20:44:54 -0400

    adding functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,130 @@
-$(document).ready(function(){
 
-// There will be four crystals displayed as buttons on the page.
- 
+$(document).ready(function () {
 
-var crystal1, crystal2, crystal3, crystal4
+    // There will be four crystals displayed as buttons on the page.
 
 
-// The player will be shown a random number at the start of the game btwn 19-120
-
-function randomNumber() {
-    var random = Math.floor(Math.random() * 120) + 19; 
-    return random
-}
-
-function gemRandomNumber() {
-    var gemRandom = Math.floor(Math.random() * 12) + 1;
-    return gemRandom
-}
-
-var gemNumber = gemRandomNumber();
-$("#score").html(gemNumber);
-
-// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-
-//onClick Event when crystal is clicked, no. of points is updated in html
+    var crystal1, crystal2, crystal3, crystal4
 
 
-$("#gem1").click(function() {
+    // The player will be shown a random number at the start of the game btwn 19-120
+
+    // function randomNumber() {
+    //     return Math.floor(Math.random() * 101) + 19;
+    // }
+
+    // function gemRandomNumber() {
+    //     return Math.floor(Math.random() * 12) + 1;
+    // }
+
+    // var targetNumber;
+    // var score;
+    // var wins = 0;
+    // var losses = 0;
+    // function Start() {
+    //     targetNumber = randomNumber();
+    //     score = 0;
+    //     $("#gem1").val(gemRandomNumber);
+    //     $("#gem2").val(gemRandomNumber);
+    //     $("#gem3").val(gemRandomNumber);
+    //     $("#gem4").val(gemRandomNumber);
+
+    //     $("#score").html(score);
+    //     $("#target").html(targetNumber);
+    // }
+    // Start();
     
-   console.log((Math.floor(Math.random() * 12) + 1)); 
-
-});
-
-$("#gem2").click(function() {
     
-    console.log((Math.floor(Math.random() * 12) + 1)); 
- 
- });
+    // $(".gem").on("click", function () {
+    //     var gem = $(this);
 
- $("#gem3").click(function() {
-    
-    console.log((Math.floor(Math.random() * 12) + 1)); 
- 
- });
+    //     score = score + parseInt(gem.val());
 
- $("#gem4").click(function() {
-    
-    console.log((Math.floor(Math.random() * 12) + 1)); 
- 
- });
-//create var to hold total score *create a new random number (loop?) for each image
-
-var targetNumber = randomNumber();
-$("#target").html(targetNumber);
-
-
-var playerScore =
+    //     $("#score").html(score);
+        
+    //     if (score > targetNumber) {
+    //         losses++;
+    //         $("#losses").text(losses)
+    //         Start();
+    //     }
+    //     else if (score === targetNumber){
+    //         wins++;
+    //         $("#wins").text(wins);
+    //         Start();
+    //     }
+    // })
 
 
-// Your game will hide this amount until the player clicks a crystal.
-    //players total score will be hidden .hide() .show()
 
-// When they do click one, update the player's score counter.
-    //onClick function - refer to hangman, update score function
+        // Your game will hide this amount until the player clicks a crystal.
+        //players total score will be hidden .hide() .show()
+
+        // When they do click one, update the player's score counter.
+        //onClick function - refer to hangman, update score function
 
 
-// The player wins if their total score matches the random number from the beginning of the game.
-    //to win, number must match randomNumber at game start
+        // The player wins if their total score matches the random number from the beginning of the game.
+        //to win, number must match randomNumber at game start
 
-    function win () {
+        function win() {
 
-    }
-// The player loses if their score goes above the random number.
+        }
+    // The player loses if their score goes above the random number.
     //if number is greater than randomNumber, player loses
 
-// The game restarts whenever the player wins or loses.
+    // The game restarts whenever the player wins or loses.
     //refer to setUpnewgame? from hangman
 
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+    // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 
 
 
     //setupnewRound - reset to 0, new randomNumber
 
-// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+    // The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
 
 
 
     //gameObj to store new values
 
-// The random number shown at the start of the game should be between 19 - 120.
+    // The random number shown at the start of the game should be between 19 - 120.
+    function randomNumber() {
+        return Math.floor(Math.random() * 101) + 19;
+    }
+    // Each crystal should have a random hidden value between 1 - 12.
+    function gemRandomNumber() {
+        return Math.floor(Math.random() * 12) + 1;
+    }
+
+    var targetNumber;
+    var score
+    var wins = 0;
+    var losses = 0;
+
+    function start () {
+        targetNumber = randomNumber();
+        $("#target").text(targetNumber);
+        score = 0;
+        $("#score").html(score);
+
+        $("#gem1").val(gemRandomNumber);
+        $("#gem2").val(gemRandomNumber);
+        $("#gem3").val(gemRandomNumber);
+        $("#gem4").val(gemRandomNumber);
+
+    }
+    start();
 
 
-// Each crystal should have a random hidden value between 1 - 12.
+    $(".gem").on("click", function(){
+        var gem = $(this);
+
+        score = score + parseInt(gem.val());
+
+        $("#score").html(score);
+
+
+    })
 })
+
